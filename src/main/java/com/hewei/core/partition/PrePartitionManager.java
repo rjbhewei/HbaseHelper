b40fb54f7a12d7e2b0815ef1adc02925c6caa5c1
rjbhewei <rjbhewei@gmail.com>
2015-12-21 17:56:42 +0800

    xxxx

---
+++ src/main/java/com/hewei/core/partition/PrePartitionManager.java
@@ -0,0 +1,16 @@
+package com.hewei.core.partition;
+
+/**
+ * @author hewei
+ * @version 5.0
+ * @date 15/12/21  17:54
+ * @desc
+ */
+public class PrePartitionManager {
+
+    private int partition;
+    private int num;
+
+
+
+}
